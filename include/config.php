fff86f3062bbde6bb78f20bfc203b4d249ee5173
Diego Carmona Bernal <cbdiegox@gmail.com>
2024-07-15 18:10:51 -0600

    mikrotik backend and front

---
+++ include/config.php
@@ -0,0 +1,16 @@
+<?php
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");};
+$data['mikhmon'] = array ('1'=>'mikhmon<|<admin','mikhmon>|>mZWfoZ8=');
+
+
+
+
+
+
+
+
+
+
+
+
+$data['SesionCBDX'] = array ('1'=>'SesionCBDX!189.148.218.83','SesionCBDX@|@admin','SesionCBDX#|#qqChrA==','SesionCBDX%HostCBDX','SesionCBDX^pruebamikrotik.freedynamicdns.net','SesionCBDX&MX','SesionCBDX*10','SesionCBDX(1','SesionCBDX)','SesionCBDX=0','SesionCBDX@!@disable');
